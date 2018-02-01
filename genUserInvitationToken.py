2a7de550486c34c792b3cc05c8d717c33e5e4abf
Sergio Cabrero <sergiocabrerobarros@gmail.com>
2018-02-01 19:49:34 +0100

    Print hostname in invitation url

--- genUserInvitationToken.py
+++ genUserInvitationToken.py
@@ -1,9 +1,9 @@
 import random
-
+from socket import gethostname
 with open('usertokens.txt', 'a+') as tokens:
     token = '%030x' % random.randrange(16**random.randint(64, 128))
     if tokens.tell(): tokens.write('\n')
 
     tokens.write(token)
 
-print 'URL: https://server/user/%s/' % token
+print 'URL: https://%s:3000/user/%s/' % (gethostname(), token)
