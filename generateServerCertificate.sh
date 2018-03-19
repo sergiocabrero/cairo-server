e3289de71cb4ff79a05cf8f0e307e48a089d862d
Sergio Cabrero <s.cabrero@cwi.nl>
2018-03-19 18:01:06 +0100

    warning on signed certificates

--- generateServerCertificate.sh
+++ generateServerCertificate.sh
@@ -1,2 +1,3 @@
+echo "If a signed certificate is needed, look for options like: https://certbot.eff.org/lets-encrypt/"
 echo "chooseserverpassphrase" > passphrase.txt
 openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -passout file:passphrase.txt -keyout key.pem -out cert.pem
